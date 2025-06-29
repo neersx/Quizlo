4aa698144a97bfc5749053591c34e5da87ba6849
Neeraj <singh.neer@ymail.com>
2025-06-29 22:24:25 +0530

    Test Type Migration completed

---
+++ Quizlo.Questionnaire.WebApi/Migrations/20250629164814_TestTypeMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quizlo.Questionnaire.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestTypeMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Tests",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Tests");
+        }
+    }
+}
