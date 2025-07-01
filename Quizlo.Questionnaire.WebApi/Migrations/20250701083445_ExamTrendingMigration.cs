a0146833f304c876725329cdfbd8a46fa1679aa8
Neeraj <singh.neer@ymail.com>
2025-07-01 14:05:51 +0530

    Migration - Exam IsTrending column

---
+++ Quizlo.Questionnaire.WebApi/Migrations/20250701083445_ExamTrendingMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quizlo.Questionnaire.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExamTrendingMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsTrending",
+                table: "Exams",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsTrending",
+                table: "Exams");
+        }
+    }
+}
