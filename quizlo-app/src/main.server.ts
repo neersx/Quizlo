cd8a71d29affe54419694762ece8cb02716bebdf
Neeraj Singh <singh.neer@ymail.com>
2025-07-01 11:23:17 +0530

    Fixed Test window design issues

--- quizlo-app/src/main.server.ts
+++ quizlo-app/src/main.server.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { config } from './app/app.config.server';
+import 'zone.js/node';
 
 const bootstrap = () => bootstrapApplication(App, config);
 
