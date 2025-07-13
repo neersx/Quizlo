a9842d7c510bee92abf5b5ecbd6f538b7a26a0af
Neeraj Singh <singh.neer@ymail.com>
2025-07-13 21:41:03 +0530

    Updated eenvironment

--- quizlo-app/src/environments/environment.ts
+++ quizlo-app/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     aiUrl: 'https://n8n.srv893077.hstgr.cloud/webhook-test/ffc4de45-e563-49db-aca4-a7e39453c9d7',
-    apiUrl: 'http://localhost:5081/api'
+    apiUrl: 'https://quizloai.com/api'
   };
