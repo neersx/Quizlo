2a216f7a6f8da9244fdee46288f1c4fac4b71561
Neeraj Singh <singh.neer@ymail.com>
2025-07-24 10:59:49 +0530

    Migrations of QuestionsInExam

---
+++ Quizlo.Questionnaire.WebApi/DTO/ExamWithEmptySubjectsDto.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quizlo.Questionnaire.WebApi.DTO
+{
+public class ExamWithEmptySubjectsDto
+{
+    public int ExamId { get; set; }
+    public string ExamName { get; set; }
+    public List<SubjectDto> Subjects { get; set; }
+}
+
+public class SubjectDto
+{
+    public int SubjectId { get; set; }
+    public string Name { get; set; }
+    public int TotalQuestions { get; set; }
+}
+
+}
---
+++ Quizlo.Questionnaire.WebApi/Migrations/20250724052652_SubjectExamQuesDbMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quizlo.Questionnaire.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SubjectExamQuesDbMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "QuestionsInExam",
+                table: "Subjects",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QuestionsInExam",
+                table: "Subjects");
+        }
+    }
+}
