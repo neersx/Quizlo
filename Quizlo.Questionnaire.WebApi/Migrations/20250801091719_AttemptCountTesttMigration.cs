9b1c2e48c38cbf8d4c8e8875c7e53fbaeb0d81d4
Neer S <singh.neer@ymail.com>
2025-08-01 19:24:14 +0530

    AttemptCount Test Migration

---
+++ Quizlo.Questionnaire.WebApi/Migrations/20250801091719_AttemptCountTesttMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quizlo.Questionnaire.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AttemptCountTesttMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AttemptCount",
+                table: "Tests",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AttemptCount",
+                table: "Tests");
+        }
+    }
+}
